fab9721a1b049d23c32081278f7859bc4b69e551
ufidon <ufidon@gmail.com>
2018-04-30 14:14:06 -0500

    CRUD for SQLite with ADO added

--- dbapps/SQLiteADO.cs
+++ dbapps/SQLiteADO.cs
@@ -34,7 +34,34 @@ namespace DbApp02
                     Console.WriteLine("Name: " + reader["full name"] + " is from " + reader["country"] 
                         + ", born on "+ DateTime.Parse(reader["Bdate"].ToString()));
 
+                reader.Close();
+
+
+                sql = @"insert into shipper values(77,'UPS','7777777777')";
+                command = new SQLiteCommand(sql, m_dbConnection);
+                int rows = command.ExecuteNonQuery();
+                Console.WriteLine(rows.ToString() + " rows affected!");
+
+
+                sql = @"update shipper set PhoneNum='8888888888' where id=77";
+                command = new SQLiteCommand(sql, m_dbConnection);
+                rows = command.ExecuteNonQuery();
+                Console.WriteLine(rows.ToString() + " rows affected!");
+
+
+                sql = @"select *  from shipper";
+                command = new SQLiteCommand(sql, m_dbConnection);
+                reader = command.ExecuteReader();
+                while (reader.Read())
+                    Console.WriteLine("ID: " + reader["id"].ToString() + " is " + reader["Name"]
+                        + " with phone number:  " + reader["PhoneNum"]);
+
+                reader.Close();
+
                 m_dbConnection.Close();
+
+
+
             }
             catch(SQLiteException e)
             {
